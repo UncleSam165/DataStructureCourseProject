53364cc022b69a3652ea93ed5ae49d192ad78a88
KholoudMahmoud1 <88489691+KholoudMahmoud1@users.noreply.github.com>
2021-08-29 16:44:19 +0200

    Update Fighter.cpp

--- Enemies/Fighter.cpp
+++ Enemies/Fighter.cpp
@@ -11,16 +11,21 @@ void Fighter::Move() {
 	DecrementDist();
 }
 
-void Fighter::Act() {
+void Fighter::Act(Enemy* E) {
 if ((int)(CurrentTimeStep - ArrvTime) % ((int)ReloadPeriod + 1) == 0) {
 		double Damage = (K * Power) / Distance;
 		NewCastle->DecrementHealth(Damage);
 	}
 
 
-       if (status != KILD) {
-		Health += GetHealthHeal();
+       if (dynamic_cast<Healer*>(E)){
+		if ((E->GetDistance() - this->Distance)<= 2){
+			if (status != KILD) {
+				Health += GetHealthHeal();
+			}
+		}
 	}
+
 }
 
 	
