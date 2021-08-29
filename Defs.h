00a4087a92d5a655185f2eb1119b423cbbb54ed5
KholoudMahmoud1 <88489691+KholoudMahmoud1@users.noreply.github.com>
2021-08-29 11:50:09 +0200

    Create Defs.h

--- Defs.h
+++ Defs.h
@@ -8,7 +8,7 @@
 								// (will be drawn vertical to each other)
 #define MaxEnemyCount 900       // the maximum number of enemies the application can work on
 
-int CurrrntTimeStep;
+int CurrentTimeStep;
 
 
 enum ENMY_TYPE	//types of enemies
